2200498019444b996d9092c88aa0fc9ad5d9be58
Marvin Ramirez <eldo@hotmail.com>
2018-07-18 00:19:52 -0400

    CreateProfile and Route commit

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/dashboard";
+import CreateProfile from "./components/creat-profile/CreateProfile";
 import "./App.css";
 import { clearCurrentProfile } from "./actions/profileActions";
 
@@ -49,6 +50,13 @@ class App extends Component {
                 <Route exact path="/login" component={Login} />
                 <Switch>
                   <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                </Switch>
+                <Switch>
+                  <PrivateRoute
+                    exact
+                    path="/create-profile"
+                    component={CreateProfile}
+                  />
                 </Switch>
               </div>
             </div>
